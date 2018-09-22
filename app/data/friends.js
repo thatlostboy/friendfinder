09a5acdd8d1bd5e8c78a49d3683dbab90391baba
Wong <thatlostboy@gmail.com>
2018-09-21 22:07:56 -0700

    Updated Questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,7 +5,16 @@
 // ===============================================================================
 
 var friendsArray = [
-
+    {
+        name: "hot guy",
+        photo: "https://media2.giphy.com/media/3ov9jVedETgcdCNjHi/giphy.gif",
+        scores: [1,1,1,1,1,1,1,1,1,1]
+    },     
+    {
+        name: "hot girl",
+        photo: "https://media1.giphy.com/media/nqO46lkGROwiA/giphy.gif",
+        scores: [5,5,5,5,5,5,5,5,5,5]
+    }
 ]
 
 module.exports = friendsArray;
